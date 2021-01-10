46dc50ad686d67c8d168e7e1e358bfcf39455e31
Marjuw <33716855+Marjuw@users.noreply.github.com>
2021-01-10 18:15:10 +0100

    Update JSON Schema

--- Code Server/Server/api/models/chats.js
+++ Code Server/Server/api/models/chats.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const chatSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    bezeichnung: String,
-    erstelldatum: String, //Schauen ob Datum als Typ ?
-    userID: [String]
+    bezeichnung: {type: String, required: true},
+    erstelldatum: {type: String, required: true}, //Schauen ob Datum als Typ ?
+    userID: {type: [Number], required: true}
 });
 
 module.exports = mongoose.model('Chats', chatSchema);
