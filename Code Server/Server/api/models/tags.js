621a8e92f24384a2c296f9186ce38f259919fcc0
Marjuw <33716855+Marjuw@users.noreply.github.com>
2021-01-10 18:04:05 +0100

    UPDATE JSON Schema

--- Code Server/Server/api/models/tags.js
+++ Code Server/Server/api/models/tags.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const tagSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
-    bezeichnung: String,
-    typ: String  // Kategorie, Skills/Anforderungen, Kommunikation, Zweck als Tag
+    bezeichnung: {type: String, required: true},
+    typ: {type: String, required: true}  // Kategorie, Skills/Anforderungen, Kommunikation, Zweck als Tag
 });
 
 module.exports = mongoose.model('tags', tagSchema);
