e2436e99a36a3039bbcc0050692b77e1e9ae8209
Sergej Ebel <sergej.ebel@smail.th-koeln.de>
2021-02-17 17:03:45 +0100

    add tag.js

---
+++ Code Server/Server/api/models/tag.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const tagSchema = mongoose.Schema({
+
+    _id: Number,
+    bezeichnung: {type: String, required: true},
+    typ: {type: String, required: true}  // Kategorie, Skills/Anforderungen, Kommunikation, Zweck als Tag
+});
+
+module.exports = mongoose.model('tag', tagSchema);
