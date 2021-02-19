766b048fdb56eb8fefc90f7bec8fa83a1b2edfb3
Sergej Ebel <sergej.ebel@smail.th-koeln.de>
2021-02-19 02:51:00 +0100

    Profiledit Update, Bezug per GET

--- Code App/PiP2/app/src/main/java/com/example/pip/ProfileEditFragment.kt
+++ Code App/PiP2/app/src/main/java/com/example/pip/ProfileEditFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.FragmentTransaction
 import com.example.REST_API_Client.*
 
@@ -46,19 +47,70 @@ class ProfileEditFragment : Fragment() {
 
          var editpersonname: EditText = v.findViewById(com.example.pip.R.id.name_eigen_feld_edit)
 
+        // HIER mein Code
+        val main: MainActivity = MainActivity()
+        main.loggedinUserID
+
+        // Einen GET auf einen spezifischen Nutzer ausführen
+        val getSpecificUserString = okHttpGet(ressource_users ,main.loggedinUserID) // hole den JSON String vom spezifischen User aus der Ressource Users
+        val loggedinUser = gson.fromJson(getSpecificUserString, One_User::class.java) // speichere den User in eine Variable vom Typ One_User
+
+        //Ab hier alle Profildaten den Mustereinträgen überschreiben
+        val nicknameProfil : TextView =  v.findViewById(R.id.nickname_eigen_feld_edit)   // nickname Textfeld identifizieren
+        nicknameProfil.setText(loggedinUser.nickname)  //Mustereintrag von Nickname zu dem Eintrag von Nickname der Get Anfrage überschreiben
+
+        val nameProfil : TextView =  v.findViewById(R.id.name_eigen_feld_edit)
+        nameProfil.setText(loggedinUser.vorname)
+
+        val nachnameProfil : TextView =  v.findViewById(R.id.nachname_eigen_feld_edit)
+        nachnameProfil.setText(loggedinUser.name)
+
+        val beschreibungProfil : TextView =  v.findViewById(R.id.beschreibung_eigen_feld_edit)
+        beschreibungProfil.setText(loggedinUser.beschreibung)
+
+        val kommunikationProfil : TextView =  v.findViewById(R.id.kommunikation_eigen_feld_edit)
+        kommunikationProfil.setText(loggedinUser.kommunikation)
+
+
+        val interessenString = mutableListOf<String>()
+        loggedinUser.interessen.forEach { x ->
+            main.tagListe?.forEach { y -> if ( y.id == x ) interessenString.add(y.bezeichnung) }
+        }
+
+        val faehigkeitenString = mutableListOf<String>()
+        loggedinUser.faehigkeiten.forEach { x ->
+            main.tagListe?.forEach{ y -> if ( y.id == x) faehigkeitenString.add(y.bezeichnung)}
+        }
+
+        val interessenProfil : TextView =  v.findViewById(R.id.interessen_eigen_feld_edit)
+        interessenProfil.setText(interessenString.joinToString(separator = ", "))
+
+        val faehigkeitenProfil : TextView =  v.findViewById(R.id.fähigkeiten_eigen_feld_edit)
+        faehigkeitenProfil.setText(faehigkeitenString.joinToString(separator = ", "))
+
 
          acceptedit.setOnClickListener(  View.OnClickListener {
 
 
+        // PUT Request auf den eingeloggten Nutzer machen
+//        val updatedUser: One_User = One_User(
+//                nickname = "berndy der Lockere",
+//                vorname = "Bernard",
+//                name = "Das Brot",
+//                interessen = listOf(1,2,3),
+//                faehigkeiten = listOf(101,102,103),
+//                email = "brotbackboy@hefe.com",
+//                passwort = "1234hehe" )
+//        val jsonUserObject2 = gson.toJson(updatedUser) // das One_User Objekt wird in einen JSON String geparset
+//        okHttpPut(ressource_users, main.loggedinUserID, jsonUserObject2) // der User wird gepostet
 
+
+
+
+             // wichtig zum wechseln
             var profileFragment: FiveFragment = FiveFragment()
 
-
-            var s =editpersonname.text.toString()
-            val bundle = Bundle()
-            bundle.putString("key",s)
-            profileFragment.setArguments(bundle)
-
+             // wichtig zum wechseln des Fragments
             var transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
             transaction.replace(this.id, profileFragment)
             transaction.commit()
